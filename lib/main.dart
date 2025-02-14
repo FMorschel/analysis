13b3411248683b2ec1c2760f646668b29c2c4add
FMorschel <52160996+FMorschel@users.noreply.github.com>
2025-02-14 07:52:39 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+// ignore: unused_import
+import 'test.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    );
+  }
+}
